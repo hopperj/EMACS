8b99c6b1c89cda5eda63781e5358b08d6f40fd3b
Jason Hopper <hopperj@Locutus.local>
2014-11-15 22:42:38 -0400

    More changes. Science.

--- SerialServer/serialServer.py
+++ SerialServer/serialServer.py
@@ -10,10 +10,12 @@ from urllib2 import Request, urlopen, URLError
 from optparse import OptionParser
 import json
 import requests
+from datetime import datetime
+from time import sleep
 
 class SerialServer:
 
-    def __init__(self, numberOfDevices=0, webAddress="http://192.168.100.207/monitor/new/", devicePrefix="/dev/ttyUSB", timeout=10.0, startCharacter='$'):
+    def __init__(self, numberOfDevices=0, webAddress="http://192.168.100.207/monitor/new/", devicePrefix="/dev/ttyUSB", device=None, timeout=10.0, startCharacter='$'):
         # Main web address
         self.webAddress = webAddress
         # Serial timeout value
@@ -27,6 +29,17 @@ class SerialServer:
 
         self.startCharacter = startCharacter
 
+        #self.run = True
+        if device is not None:            
+            self.device = device
+            self.connect( self.device )
+        else:
+            self.connectSerial()
+
+
+    def connect(self, device):
+        ser = serial.Serial( device, baudrate=9600, timeout=10.0)
+        self.serialDevices = [ ser ]
 
     def closeAll(self):
         """
@@ -51,6 +64,7 @@ class SerialServer:
         self.serialDevices = []
         for i in range(self.numberOfDevices):
             ser = serial.Serial( self.devicePrefix+str(i), baudrate=9600, timeout=10.0)
+            print "Added %d serial device"%i
             self.serialDevices.append( ser )
 
 
@@ -62,24 +76,29 @@ class SerialServer:
         data = {}
         try:
             ser.write('$')
-            tmpData = ser.read(4096*5)
+            tmpData = ser.read(100)
+            print "Got data"
         except serial.SerialTimeoutException:
             # Return 1 indicating error and None as the data
             return [1, None]
 
-        for ele in tmpData.split("\n"):
+        print tmpData
+
+        for ele in tmpData.split(","):
             k,v = ele.split(':')
-            data[k] = float(v)
+            data[k] = v
 
 
-        return data
+        return [0, data]
 
 
     def sendToWeb(self, data):
         """
         curl -H 'Content-Type: application/json' -d '{"device": 1, "sensor_name": "temp1", "value": 22, "created_at": "1000000987"}' 127.0.0.1:8000/monitor/new/
         """
+        # Put data into a json object
         payload = json.dumps(data)
+        # Build headers
         headers = {'content-type': 'application/json'}
 
         response = requests.put(self.webAddress, data=payload, headers=headers)
@@ -96,7 +115,8 @@ class SerialServer:
 
         # Get rid of the last
         data = data[-1]
-        ser.write(res)
+        ser.write('@')
+        ser.write(data)
 
     def run(self):
         """
@@ -104,11 +124,23 @@ class SerialServer:
         getting data, sending to web server, get control data
         and send back to arduino.
         """
-        for ser in self.serialDevices:
-            err, data = self.getData(ser)
-            if not err and data is not None:
-                res = self.sendToWeb( data )
-                self.sendToSerial( ser, res )
+        while 1:
+            print "Looping"
+            d0 = datetime.now()
+            for ser in self.serialDevices:
+                print "Getting data from Arduino"
+                err, data = self.getData(ser)
+                if not err and data is not None:
+                    print "Sending data to website"
+                    res = self.sendToWeb( data )
+                    print "Sending control data back to arduino"
+                    self.sendToSerial( ser, res )
+                else:
+                    print "There was an error"
+                    print err, data
+            dt = ( datetime.now() - d0 ).total_seconds()
+            if dt > 10:
+                sleep( 10 - dt ) 
 
 
 if __name__ == "__main__":
@@ -118,12 +150,19 @@ if __name__ == "__main__":
                       help="Number of devices connected")
     parser.add_option("-s", "--server", dest="server",
                       help="Server to send data too")    
-    parser.add_option("-q", "--quiet",
-                      action="store_false", dest="verbose", default=True,
-                      help="don't print status messages to stdout")
-
+    parser.add_option("-p", "--prefix", dest="devicePrefix",
+                      help="Prefix for devices")
+    parser.add_option("--serial", dest="serial",
+                      help="Specific serial device")
     (options, args) = parser.parse_args()
 
-    SS = SerialServer(numberOfDevices = options.noOfDevices, webAddress=options.server)
-    data = {"device": 1, "sensor_name": "temp1", "value": 22, "created_at": "1000000987"}
-    SS.sendToWeb( data )
+    SS = SerialServer(numberOfDevices = int(options.noOfDevices), webAddress=options.server, device=options.serial)
+    #data = {"device": 1, "sensor_name": "temp1", "value": 22, "created_at": "1000000987"}
+    #res = SS.sendToWeb( data )
+    #SS.sendToSerial( ser, res )
+    print "Going into run mode"
+    try:
+        SS.run()
+    except KeyboardInterrupt:
+        #SS.run = False
+        SS.closeAll()
